345072fa75bee0285b5bd2fefc32a9a71b2ecdf3
Nelmangx <164669023+Nelmangx@users.noreply.github.com>
2025-06-07 09:14:08 -0300

    feat: atividade 01

---
+++ lista-php-1/exc01.php
@@ -0,0 +1,8 @@
+<?php
+    //ultilizando valores personalizados. para personalizar ultilize ?a = "valor desejado"
+    $n1 = $GET_['a'];
+    $n2 = $GET_['b'];
+    $resultado = $n1 * $n2;
+
+    echo "o resultado da multilicação de $n1 e $n2 é $resultado.";
+?>
